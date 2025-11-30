ae1c4d3e24399e70c1e5109b8e3167ba68b0652e
guyt6517b <maasben@usd437.net>
2025-11-30 13:01:16 -0600

    Add files via upload

---
+++ terminal/operations/periodic table/entrypoint.py
@@ -0,0 +1,11 @@
+import sys
+import time
+import os
+import math
+from operations.operations.helpers import callconversions, clearscreen, colors, printDisclaimer
+from operations.operations.helpers.pyon import pyon
+from operations.operations import bohrmodels, generalinfo, ioninfo, isotopeinfo, molarmass
+
+printDisclaimer._()
+
+global mapping
